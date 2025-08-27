4d68d863f060281f6ae5f5b0407f3c5b8a72c8b0
Amine Dif <81390186+xon-nate@users.noreply.github.com>
2025-08-27 13:29:40 +0100

    m

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ const addEventOnElements = function (elements, eventType, callback) {
 const preloader = document.querySelector("[data-preloader]");
 
 window.addEventListener("load", function () {
-  preloader.classList.add("loaded");
+  if (preloader) preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
 
