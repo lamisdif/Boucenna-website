1c76438ba9ce9a4599caa751b38f9bf0446cfaac
Lamis Dif <lamisdif@gmail.com>
2026-08-30 01:13:48 +0200

    i hope last

--- app.js
+++
@@ -1,8 +0,0 @@
-const questions = document.querySelectorAll('.faq-question');
-
-questions.forEach(question => {
-    question.addEventListener('click', () => {
-        const answer = question.nextElementSibling;
-        answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
-    });
-});
